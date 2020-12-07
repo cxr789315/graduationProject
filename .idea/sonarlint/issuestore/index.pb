51320b16c3757fb25005c744222854b708b44a02
PC-202009241301\Administrator <xiangang.cheng@eyunsong.com>
2020-12-07 17:45:49 +0800

    创建项目

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/main/webapp/WEB-INF/web.xml,2\e\2e22b6edd746fa3dc40ad7276c87c8d87d72a392
+Q
+!src/test/controllerTest/Test.java,1\7\17b904bca00027f610f4c97771beec7dfe11f5f9
---
+++ src/test/controllerTest/Test.java
@@ -0,0 +1,16 @@
+package controllerTest;
+
+/**
+ * -
+ * @author chengxiangang
+ * @version V 1.0
+ * @project agricultural
+ * @date 2020/12/7 16:42
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test1() {
+        System.out.println("测试");
+    }
+}
